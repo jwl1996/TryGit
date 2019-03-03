9c506f95d47522ce1b862e5818568c4d24f8419a
wl <739048569@qq.com>
2019-03-03 21:04:49 +0800

    geng zheng

--- TryGit/src/com/wl/trygit/TryGit.java
+++ TryGit/src/com/wl/trygit/TryGit.java
@@ -3,6 +3,6 @@ package com.wl.trygit;
 public class TryGit {
 	public static void main(String[] args) {
 		System.out.println("hello hhha xixihei...");
-		Sustem.out.print("修改后，返回去。。。");
+		System.out.print("修改后，返回去。。。");
 	}
 }
